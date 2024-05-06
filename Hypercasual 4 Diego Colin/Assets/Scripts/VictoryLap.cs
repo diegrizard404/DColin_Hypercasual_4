38b3e64cfb235c941226c308e27d74ac5b0a254b
EdMT220 <158091912+EdMT220@users.noreply.github.com>
2024-05-06 09:26:29 -0600

    v03

--- Hypercasual 4 Diego Colin/Assets/Scripts/VictoryLap.cs
+++ Hypercasual 4 Diego Colin/Assets/Scripts/VictoryLap.cs
@@ -7,6 +7,24 @@ using UnityEngine.SceneManagement;
 public class VictoryLap : MonoBehaviour
 {
     [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject cerdo01;
+    [SerializeField] GameObject cerdo02;
+    [SerializeField] GameObject castle1;
+    [SerializeField] GameObject castle2;
+
+    private void Update()
+    {
+        if (cerdo01 == null)
+        {
+            castle1.SetActive(false);
+            castle2.SetActive(true);
+        }
+        if (cerdo02 == null)
+        {
+            castle2.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
